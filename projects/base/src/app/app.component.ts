517e50a5192182899d452f343dc205fb127de9b8
alexander kozlov <akozlov@autospot.ru>
2023-03-29 10:21:15 +0300

    update font, header++

--- projects/base/src/app/app.component.ts
+++ projects/base/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {Store} from "@ngrx/store";
 import {countSelector, decrease, increase, updatedAtSelector} from "./reducers/counter";
 import {map} from "rxjs";
+import { IHeaderNavigate } from '../../../ui/src/lib/components/header/models/navigate';
 
 @Component({
   selector: 'base-root',
@@ -16,10 +17,14 @@ export class AppComponent {
   );
   public updatedAt$ = this.store.select(updatedAtSelector);
 
+  public navigate: IHeaderNavigate[] = [
+    { name: 'Home', uri: '' },
+    { name: 'Components', uri: 'components' },
+    { name: 'Contacts', uri: 'Contacts' },
+  ]
+
   constructor(private store: Store) {
   }
-
-
 
   public increase() {
     this.store.dispatch(increase())
